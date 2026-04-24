677e1dba65b027b9f747b138c3cc66679ad1195f
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2026-04-24 03:04:47 +0200

    Custom app titlebar #28 access to os for mac optimization

--- src/preload.js
+++ src/preload.js
@@ -3,5 +3,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
     triggerUpdateCheck: (isAutoUpdateEnabled) => ipcRenderer.send('check-updates', isAutoUpdateEnabled),
-    getAppVersion: () => ipcRenderer.invoke('get-app-version')
+    getAppVersion: () => ipcRenderer.invoke('get-app-version'),
+    getPlatform: () => process.platform
 });
