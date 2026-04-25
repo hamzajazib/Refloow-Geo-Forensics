3d281d1f737c2ccc04979ad5664fc8d978db1cbd
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2026-04-25 15:15:08 +0200

    Add selectFolder method in preload for #24

--- src/preload.js
+++ src/preload.js
@@ -4,5 +4,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electronAPI', {
     triggerUpdateCheck: (isAutoUpdateEnabled) => ipcRenderer.send('check-updates', isAutoUpdateEnabled),
     getAppVersion: () => ipcRenderer.invoke('get-app-version'),
-    getPlatform: () => process.platform
+    getPlatform: () => process.platform,
+    selectFolder: () => ipcRenderer.invoke('dialog:openDirectory')
 });
